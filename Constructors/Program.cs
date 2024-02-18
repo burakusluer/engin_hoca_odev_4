d29f5c295ca6390e2b06730935531b722f1907fa
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-18 20:10:01 +0300

    Constructors project added (second lesson)

---
+++ Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿Customer customer=new Customer() {Id=1,FirstName="Engin",LastName="Demiroğ",City="Ankara" };
+Customer customer3 = new Customer();
+Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara");
+
+Console.WriteLine(customer2.FirstName);
+class Customer
+{
+    public Customer()
+    {
+        
+    }
+    public Customer(int id,string FirstName,string LastName,string City)
+    {
+        this.Id = id;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.City = City;
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
