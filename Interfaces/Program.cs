d45f94c0f86977a3594e9a5287259a3d4c7b3e3f
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-18 20:58:12 +0300

    3.lesson Intefaces added

---
+++ Interfaces/Program.cs
@@ -0,0 +1,57 @@
+﻿IPersonManager employeeManager=new EmployeeManager();//interface ve abstractlar newlenemez
+IPersonManager customerManager=new CustomerManager();//interface ve abstractlar newlenemez
+ProjectManager projectManager=new ProjectManager();
+projectManager.Add(customerManager);
+
+interface IPersonManager
+{
+	public void Add();
+	public void Update();
+}
+class CustomerManager : IPersonManager
+{
+    public int denem { get; set; }
+    public void Add()
+    {
+        //Müşteri Ekleme Kodları
+        Console.WriteLine("Müşteri Eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Müşteri Güncellendi");
+    }
+}
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        //Çalışan Ekleme Kodları
+        Console.WriteLine("Çalışan Eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Çalışan Güncellendi");
+    }
+}
+
+class InternManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Stajyer Eklendi");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Stajyer Güncellendi");
+    }
+}
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+}
